18e9f6c1753760e8721c861179c0262c66c1a0dd
Stefan <88635336+ThalesCRG@users.noreply.github.com>
2022-07-28 12:10:44 +0200

    Update Command Type
    https://discord.com/developers/docs/interactions/application-commands

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,10 +2,19 @@ export interface Command {
   name: string;
   description: string;
   options?: Options[];
+  guildId?: string;
+  default_member_permissions?: string;
+  dm_permission?: boolean;
 }
 export interface Options {
   name: string;
   description: string;
   type: number;
-  required: boolean;
+  required?: boolean;
+  choices?: Choices[];
+}
+export interface Choices {
+  name: string;
+  type: number;
+  value?: string | number;
 }
